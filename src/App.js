e0a3260242b183212e377b34350c61db515608c1
anprivitera <andrea.privitera@mailbox.org>
2023-08-31 22:11:40 -0700

    rendered square with function

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function Board() {
   return (
   <>
     <div className="board-row">
-      <button className="square">1</button>
+      <Square />
       <button className="square">2</button>
       <button className="square">3</button>
     </div>
