e1eeb73d42731fdd7596a159d59d824fdafbc9b8
anprivitera <andrea.privitera@mailbox.org>
2023-08-31 22:13:03 -0700

    changed all buttons to square function

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ export default function Board() {
   <>
     <div className="board-row">
       <Square />
-      <button className="square">2</button>
-      <button className="square">3</button>
+      <Square />
+      <Square />
     </div>
     <div className="board-row">
-      <button className="square">4</button>
-      <button className="square">5</button>
-      <button className="square">6</button>
+      <Square />
+      <Square />
+      <Square />
     </div>
     <div className="board-row">
-      <button className="square">7</button>
-      <button className="square">8</button>
-      <button className="square">9</button>
+      <Square />
+      <Square />
+      <Square />
     </div>
   </>
   );
