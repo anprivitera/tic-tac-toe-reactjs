db80b82afa81e132a2957e39be4ba30091e0518e
anprivitera <andrea.privitera@mailbox.org>
2023-08-31 21:46:02 -0700

    divided into first row

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 export default function Square() {
   return (
   <>
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
+    <div className="board-row">
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </div>
     <button className="square">X</button>
     <button className="square">X</button>
     <button className="square">X</button>
