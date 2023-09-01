4da8da8757ae5eb04a797798bb4996b0bac2be21
anprivitera <andrea.privitera@mailbox.org>
2023-08-31 23:07:04 -0700

    X appears when square clicked

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
 function Square() {
-  const [value, setValue] = useState(null);
+  const [squares, setSquares] = useState(Array(9).fill(null));
     function handleClick(){
-      console.log("clicked!");
+      setValue("X");
     }
   return (
   <button 
@@ -21,18 +21,18 @@ export default function Board() {
   return (
   <>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[0]} />
+      <Square value={squares[1]} />
+      <Square value={squares[2]} />
     </div>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[3]} />
+      <Square value={squares[4]} />
+      <Square value={squares[5]} />
     </div>
     <div className="board-row">
-      <Square />
-      <Square />
+      <Square value={squares[6]} />
+      <Square value={squares[7]} />
       <Square />
     </div>
   </>
