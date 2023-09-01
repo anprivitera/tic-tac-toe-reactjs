31ad9ee4f8c9e24a77a197ad45bc2ea94558c454
anprivitera <andrea.privitera@mailbox.org>
2023-08-31 21:46:32 -0700

    added numbers to first row

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ export default function Square() {
   return (
   <>
     <div className="board-row">
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
+      <button className="square">1</button>
+      <button className="square">2</button>
+      <button className="square">3</button>
     </div>
     <button className="square">X</button>
     <button className="square">X</button>
