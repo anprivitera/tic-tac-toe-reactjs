291d975bd546dee3a4bc887340f4a6297950b42e
anprivitera <andrea.privitera@mailbox.org>
2023-09-02 08:01:12 -0700

    added property i to handleClick function

--- src/App.js
+++ src/App.js
@@ -11,15 +11,17 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   // the first element in the array will be the current state of the board, the second ("setSquares") changes the value based on the function setSquares. Through array destructuring, we declare that squares (i.e., the initial state) is an array of 9 items, each one with the value of null.
 
-  function handleClick() { // declaring handleClick function
+  function handleClick(i) { // declaring handleClick function with argument i
     const nextSquares = squares.slice(); // nextSquares is now a shallow copy of the squares array
-    nextSquares[0] = "X" // the position 0 of the nextSquares array is changed from null to the string "X"
+    nextSquares[i] = "X" // the position i of the nextSquares array is changed from null to the string "X"
     setSquares(nextSquares) // tells React to change the initial state of position 0 in the squares array to "X" through useState
   }
 
   return (
   <> 
-  {/* Here we are creating three div elements called board-row. In each of the three board-rows we are adding three React elements generated from the function square to form the tic-tac-toe board. Each element takes one array position as value, and the function handleClick as onSquareClick. But even if we add onSquareClick={handleClick} to each squares, still only the first square changes from null to "X". that's because of line 16.  */}
+  {/* Here we are creating three div elements called board-row. In each of the three board-rows we are adding three React elements generated from the function square to form the tic-tac-toe board. Each element takes one array position as value, and the function handleClick as onSquareClick. 
+  
+  Right now nothing changes because "i" is not connected with anything */}
     <div className="board-row">
       <Square value={squares[0]} onSquareClick={handleClick} />
       <Square value={squares[1]} onSquareClick={handleClick} />
