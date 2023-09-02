50aa115c1d7ba4ff8015bd61c0f966308c53611a
anprivitera <andrea.privitera@mailbox.org>
2023-09-02 06:36:55 -0700

    testing use of br

--- src/Ciao.js
+++ src/Ciao.js
@@ -2,8 +2,8 @@ export default function Ciao() {
     return (
     <>
         <h1>Tic-Tac-Toe on React</h1>
-        I'm trying to create tic-tac-toe on react while goofing around with other modules of React.
-        <br />
+        I'm trying to create tic-tac-toe on react while goofing around with other modules of React. <br /><br />
+        Ciao come stai?
      </>
      );
 }
--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ root.render(
   <StrictMode>
     <Ciao />
     <br />
+    <br />
+    <br />
     <App />
   </StrictMode>
 );
